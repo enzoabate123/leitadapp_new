7399a1aa644a738cb8bb88bca209067c48b921c9
Enzo <enzoabate12345678910@gmail.com>
2026-06-01 21:46:58 -0300

    chore: change frontend dev server port to 3002

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 3000,
+    port: 3002,
     strictPort: true,
     host: true // Expõe o servidor na rede local do Raspberry Pi
   }
